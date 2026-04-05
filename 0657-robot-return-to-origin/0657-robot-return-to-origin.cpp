425104ee37f737d204b7d53d2b15d2037f74a0f3
Harsh Prajapati <harsh.prajapati22@gmail.com>
2026-04-05 19:19:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -3,9 +3,7 @@ public:
     bool judgeCircle(string moves) {
         int n= moves.length();
         int ucount=0;
-        int dcount=0;
         int lcount=0;
-        int rcount=0;
 
         for(int i=0;i<n;i++){
             if(moves[i]=='U'){
